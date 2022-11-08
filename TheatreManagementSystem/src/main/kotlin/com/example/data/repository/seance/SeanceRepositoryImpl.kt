083e7479c58081c2ac9f9fe2d83b9495bd629dcc
Lord225 <zlotymaciej@gmail.com>
2022-11-08 17:55:06 +0100

    Fixing MORE compilation errors

--- TheatreManagementSystem/src/main/kotlin/com/example/data/repository/seance/SeanceRepositoryImpl.kt
+++ TheatreManagementSystem/src/main/kotlin/com/example/data/repository/seance/SeanceRepositoryImpl.kt
@@ -2,27 +2,24 @@ package com.example.data.repository.seance
 
 import com.example.data.db.DatabaseFactory
 import com.example.data.db.extension.toSeance
+import com.example.data.db.schemas.ScheduleTable
 import com.example.data.db.schemas.SeanceTable
 import com.example.data.model.Seance
 import com.example.data.request.SeanceRequest
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.statements.InsertStatement
+import java.time.LocalDate
+import java.time.LocalTime
 
 class SeanceRepositoryImpl: SeanceRepository {
 
     override suspend fun addSeance(seanceRequest: SeanceRequest): Seance? {
-        var statement: InsertStatement<Number>? = null
-
-        DatabaseFactory.dbQuery {
-            statement = SeanceTable.insert {
-                it[title] = seanceRequest.title
-                it[genre] = seanceRequest.genre
-                it[director] = seanceRequest.director
-                it[duration] = seanceRequest.duration
-                it[price] = seanceRequest.price
-                /*it[scheduleId] = seanceRequest.scheduleId
-                it[hallId] = seanceRequest.scheduleId*/
+        var statement = DatabaseFactory.dbQuery {
+            SeanceTable.insert {
+                it[PerformanceId]  = seanceRequest.PerformanceId
+                it[seanceDate] = LocalDate.parse(seanceRequest.seanceDate)
+                it[seanceTime] = LocalTime.parse(seanceRequest.seanceTime)
             }
         }
 
