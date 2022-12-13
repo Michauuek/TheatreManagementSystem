79539c260069d5e7ab921922188eaa333d3dc019
Michauuek <michal.rzemieniuk@gmail.com>
2022-12-13 09:42:44 +0100

    Add Performance Screen

---
+++ TheatreManagementSystem/client/src/components/performance/PerformanceScreen.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+
+import Button from "@mui/material/Button";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { SeanceForm } from "../common/SeanceForm";
+import {
+  hallProps,
+  hallWithSeance,
+  performanceProps,
+  seanceProps,
+  seatState,
+} from "../db/DBModel";
+import NavbarFun from "../common/NavbarFun";
+import { PerformanceCard } from "../performance/PerformanceCard";
+import "./styles.css";
+import Banner from "../common/Banner";
+import Footer from "../common/Footer";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { getPerformance } from "../db/performanceAPI";
+import { AddSeance, getSeances } from "../db/seanceAPI";
+import { HallDisplay } from "../common/HallDisplay";
+
+export default function PerformanceScreen() {
+  const [result, setResult] = useState<performanceProps[]>([]);
+
+  useEffect(() => {
+    getPerformance().then((data) => {
+      setResult(data);
+    });
+  }, []);
+
+
+  return (
+    <div>
+      <Banner />
+      <NavbarFun />
+
+      <div className="App">
+        <Container>
+        <div className="leading-header">
+            <h1>Nadchodzące przedstawienia</h1>
+        </div>
+          <Col>
+            {result?.map((value) => {
+              return (
+                <PerformanceCard
+                  title={value.title}
+                  description={value.description}
+                  imageUrl={value.imageUrl}
+                />
+              );
+            })}
+          </Col>
+        </Container>
+      </div>
+      <Footer />
+    </div>
+  );
+}
