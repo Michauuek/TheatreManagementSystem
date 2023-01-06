c8ec0ab48140002ebbcfe12796f4ccf5009be723
Lord225 <zlotymaciej@gmail.com>
2023-01-06 23:04:39 +0100

    It works but bearly

---
+++ TheatreManagementSystem/client/src/components/common/Loading.tsx
@@ -0,0 +1,20 @@
+import { CircularProgress } from "@mui/material"
+
+
+
+export type Props = {
+    // Component if loading 
+    isLoading: boolean;
+    // Component to display if not loading
+    children: React.ReactNode;
+}
+
+
+export const LoadingSpinner = (props: Props) => {
+    // spinner using mui
+    return (
+        <div>
+            {props.isLoading ? <CircularProgress /> : props.children}
+        </div>
+    )
+}
